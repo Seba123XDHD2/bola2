5d96aebfbbfb1f53038b8dcbaf21c11e814bf943
Seba123XDHD2 <66489459+Seba123XDHD2@users.noreply.github.com>
2020-06-19 12:28:13 -0400

    Add files via upload

---
+++ RotacionTeclado.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionTeclado : MonoBehaviour
+{
+
+    public float vel = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0f, 0f, vel * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0f, 0f, -vel  * Time.deltaTime);
+        }
+    }
+}
